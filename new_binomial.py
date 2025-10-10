49d714c994b290982790849a5490adde5506b022
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2025-10-10 10:15:22 +0300

    Update new_binomial.py

--- new_binomial.py
+++ new_binomial.py
@@ -1,17 +1,17 @@
 from math import comb
 
-# Входни данни
+# Input data
 p = float(input("Enter probability chance (%): ")) / 100
 n = int(input("Enter range of Set: "))
 
 print("-" * 112)
 
-# Изчисляване на вероятностите
+# Calculate probabilities
 for k in range(n + 1):
     probability = (p ** k) * ((1 - p) ** (n - k)) * comb(n, k)
-    print(f"Success attempts: {k}  ---  Probability: {probability * 100} % | --> 1 to {1 / probability:_.2f}")
+    print(f"Success attempts: {k}  ---  Probability: {probability * 100} % | --> 1 in {1 / probability:_.2f}")
 
-# Статистически характеристики
+# Statistical characteristics
 mean = n * p
 variance = n * p * (1 - p)
 std_dev = variance ** 0.5
@@ -19,6 +19,5 @@ std_dev = variance ** 0.5
 print("-" * 112)
 print(f"Mean --- [{p * 100:.3f} % / {n:_}] events is: {mean:.4f}")
 print(f"Variance --- [{p * 100:.3f} % / {n:_}] events is: {variance:.4f}")
-print(f"Std dev --- [{p * 100:.3f} % / {n:_}] events is: {std_dev:.4f}")
+print(f"Standard deviation --- [{p * 100:.3f} % / {n:_}] events is: {std_dev:.4f}")
 print("-" * 112)
-
