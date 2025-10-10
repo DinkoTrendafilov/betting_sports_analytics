2fc48dfa4ecbd38b7ae1cc3d7c97c7eb306af0ce
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2025-10-10 10:14:31 +0300

    Update negative_binomial.py

--- negative_binomial.py
+++ negative_binomial.py
@@ -1,21 +1,21 @@
 from math import comb
 
-# Входни данни
+# Input data
 p = float(input("Enter probability chance (%): ")) / 100
 r = int(input("Enter target number of successes (r): "))
 k_max = int(input("Enter maximum number of trials to calculate: "))
 
 print("-" * 113)
 
-# Изчисляване на вероятностите
+# Calculate probabilities
 cumulative = 0
 for k in range(r, k_max + 1):
     probability = comb(k - 1, r - 1) * (p ** r) * ((1 - p) ** (k - r))
     cumulative += probability
     print(f"Trials needed: {k}  ---  Probability: {probability * 100} % | "
-          f"Cumulative: {cumulative * 100} % | --> 1 to {1 / probability:_.2f}")
+          f"Cumulative: {cumulative * 100} % | --> 1 in {1 / probability:_.2f}")
 
-# Статистически характеристики
+# Statistical characteristics
 mean = r / p
 variance = r * (1 - p) / (p ** 2)
 std_dev = variance ** 0.5
@@ -23,5 +23,5 @@ std_dev = variance ** 0.5
 print("-" * 113)
 print(f"Mean --- {r} successes with {p * 100:.3f}% probability: {mean:.4f} trials")
 print(f"Variance --- {r} successes with {p * 100:.3f}% probability: {variance:.4f}")
-print(f"Std dev --- {r} successes with {p * 100:.3f}% probability: {std_dev:.4f}")
+print(f"Standard deviation --- {r} successes with {p * 100:.3f}% probability: {std_dev:.4f}")
 print("-" * 113)
