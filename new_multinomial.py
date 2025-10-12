abc773ad5c623460bd72fc63d6fe0c239cab7fe5
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2025-10-12 17:51:10 +0300

    Update new_multinomial.py

--- new_multinomial.py
+++ new_multinomial.py
@@ -20,17 +20,19 @@ loss /= 100
 # a = 50%  / 0.5   b = 30% / 0.3  c = 20% / 0.2   k = 3
 k = 3
 d = defaultdict(float)
+potential_points = defaultdict(float)
 TOTAL_EVENTS_FACTORIAL = factorial(n)
 
+print("=" * 112)
 for a in range(n + 1):
     for b in range(n + 1):
-        for c in range(n + 1):
-            if a + b + c == n:
-                coefficient = TOTAL_EVENTS_FACTORIAL // (factorial(a) * factorial(b) * factorial(c))
-                result = (win ** a) * (draw ** b) * (loss ** c) * coefficient
-                res = (a, b, c)
-                d[res] += result
-                print(res, coefficient)
+        c = n - a - b
+        coefficient = TOTAL_EVENTS_FACTORIAL // (factorial(a) * factorial(b) * factorial(c))
+        result = (win ** a) * (draw ** b) * (loss ** c) * coefficient
+        res = (a, b, c)
+        d[res] += result
+        points = (a * 3) + b
+        potential_points[points] += result
 
 sorted_d = dict(sorted(d.items(), key=lambda kvp: (-kvp[1], kvp[0])))
 
@@ -42,4 +44,10 @@ for (key, value) in sorted_d.items():
     print(f"#{counter:_} ({key[0]}, {key[1]}, {key[2]}) -> {value * 100} %")
 print()
 print(f"Total unique combinations is: {comb(n + k - 1, n):_}")
+print("=" * 112)
+print("-" * 112)
+sorted_potential_points = dict(sorted(potential_points.items(), key=lambda kvp: (-kvp[1], kvp[0])))
+
+for (points, chance) in sorted_potential_points.items():
+    print(f"Points: {points:_} - {chance * 100} % (chance) ")
 print("-" * 112)
